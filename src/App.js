06f2a518f6017bce2ab11adf397b4cb28e66ef9a
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-08-07 16:52:47 +0800

    before start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+
+import AppSrc from "./AppSrc";
 // CONTEXTS
 import ThemeContextProvider from "./contexts/ThemeContext";
 import DataContextProvider from "./contexts/DataContext";
@@ -7,9 +9,7 @@ import QueryContextProvider from "./contexts/QueryContext";
 import UIContextProvider from "./contexts/UIContext";
 import CurrentNoteContextProvider from "./contexts/CurrentNoteContext";
 
-
 import "./resets.css";
-import AppSrc from "./AppSrc";
 
 export default function App() {
     
